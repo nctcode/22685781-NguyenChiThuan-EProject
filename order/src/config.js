31d92d75c921fea5872ea5f2b40ea1ba56e9f783
MR. NAM <thuan29032004@gmail.com>
2025-10-09 12:10:20 +0700

    Update config order

--- order/src/config.js
+++ order/src/config.js
@@ -16,7 +16,7 @@ console.log('PORT:', process.env.PORT);
 
 module.exports = {
     mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
-    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://localhost',
+    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://guest:guest@rabbitmq:5672', // SỬA DÒNG NÀY
     rabbitMQQueue: 'orders',
     port: process.env.PORT || 3002
 };
