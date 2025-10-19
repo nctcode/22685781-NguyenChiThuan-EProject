5d84438b793d2771a87c218e6f491235ea07da62
MR. NAM <thuan29032004@gmail.com>
2025-10-20 00:14:27 +0700

    Upload ci-cd

--- order/src/config.js
+++ order/src/config.js
@@ -16,7 +16,7 @@ console.log('PORT:', process.env.PORT);
 
 module.exports = {
     mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
-    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://guest:guest@rabbitmq:5672', // SỬA DÒNG NÀY
+    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://rabbitmq:5672',
     rabbitMQQueue: 'orders',
     port: process.env.PORT || 3002
 };
