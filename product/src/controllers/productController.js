c944b438487310a6c93f2bbdba9ee02d9c310626
MR. NAM <thuan29032004@gmail.com>
2025-10-20 17:07:27 +0700

    Update CI/CD

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -109,39 +109,6 @@ class ProductController {
             res.status(500).json({ message: "Server error" });
         }
     }
-    async getOrdersByProductId(req, res, next) {
-        try {
-            const token = req.headers.authorization;
-            if (!token) return res.status(401).json({ message: "Unauthorized" });
-
-            const { id } = req.params; // id = productId
-
-            // Lấy thông tin sản phẩm từ ProductService
-            const product = await this.productService.getProductById(id);
-            if (!product) {
-                return res.status(404).json({ message: "Product not found" });
-            }
-
-            // Lọc tất cả order trong ordersMap có chứa sản phẩm này
-            const orders = Array.from(this.ordersMap.values()).filter(order =>
-                order.products.some(p => p._id.toString() === id)
-            );
-
-            if (orders.length === 0) {
-                return res.status(404).json({ message: "No orders found for this product" });
-            }
-
-            res.status(200).json({
-                product,
-                orders
-            });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Server error" });
-        }
-    }
-
-
 }
 
 module.exports = ProductController;
