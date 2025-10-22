301069ff6c102076f7598e010e389e27f6dae23c
MR. NAM <thuan29032004@gmail.com>
2025-10-22 11:26:57 +0700

    Update CI/CD

--- auth/src/test/authController.test.js
+++ auth/src/test/authController.test.js
@@ -8,72 +8,72 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe("User Authentication", () => {
-  let app;
+    let app;
 
-  before(async () => {
-    app = new App();
-    await app.connectDB();
-    app.start();
-  });
-
-  after(async () => {
-    await app.authController.authService.deleteTestUsers();
-    await app.disconnectDB();
-    app.stop();
-  });
-
-  describe("POST /register", () => {
-    it("should register a new user", async () => {
-      const res = await chai
-        .request(app.app)
-        .post("/register")
-        .send({ username: "testuser", password: "password" });
-
-      expect(res).to.have.status(200);
-      expect(res.body).to.have.property("_id");
-      expect(res.body).to.have.property("username", "testuser");
+    before(async() => {
+        app = new App();
+        await app.connectDB();
+        app.start();
     });
 
-    it("should return an error if the username is already taken", async () => {
-      const res = await chai
-        .request(app.app)
-        .post("/register")
-        .send({ username: "testuser", password: "password" });
-
-      expect(res).to.have.status(400);
-      expect(res.body).to.have.property("message", "Username already taken");
-    });
-  });
-
-  describe("POST /login", () => {
-    it("should return a JWT token for a valid user", async () => {
-      const res = await chai
-        .request(app.app)
-        .post("/login")
-        .send({ username: "testuser", password: "password" });
-
-      expect(res).to.have.status(200);
-      expect(res.body).to.have.property("token");
+    after(async() => {
+        await app.authController.authService.deleteTestUsers();
+        await app.disconnectDB();
+        app.stop();
     });
 
-    it("should return an error for an invalid user", async () => {
-      const res = await chai
-        .request(app.app)
-        .post("/login")
-        .send({ username: "invaliduser", password: "password" });
+    describe("POST /register", () => {
+        it("should register a new user", async() => {
+            const res = await chai
+                .request(app.app)
+                .post("/register")
+                .send({ username: "testuser1", password: "password1" });
 
-      expect(res).to.have.status(400);
-      expect(res.body).to.have.property("message", "Invalid username or password");
+            expect(res).to.have.status(200);
+            expect(res.body).to.have.property("_id");
+            expect(res.body).to.have.property("username", "testuser1");
+        });
+
+        it("should return an error if the username is already taken", async() => {
+            const res = await chai
+                .request(app.app)
+                .post("/register")
+                .send({ username: "testuser1", password: "password1" });
+
+            expect(res).to.have.status(400);
+            expect(res.body).to.have.property("message", "Username already taken");
+        });
     });
 
-    it("should return an error for an incorrect password", async () => {
-      const res = await chai
-        .request(app.app)
-        .post("/login")
-        .send({ username: "testuser", password: "wrongpassword" });
+    describe("POST /login", () => {
+        it("should return a JWT token for a valid user", async() => {
+            const res = await chai
+                .request(app.app)
+                .post("/login")
+                .send({ username: "testuser1", password: "password1" });
 
-      expect(res).to.have.status(400);
-      expect(res.body).to.have.property("message", "Invalid username or password");
+            expect(res).to.have.status(200);
+            expect(res.body).to.have.property("token");
+        });
+
+        it("should return an error for an invalid user", async() => {
+            const res = await chai
+                .request(app.app)
+                .post("/login")
+                .send({ username: "invaliduser1", password: "password1" });
+
+            expect(res).to.have.status(400);
+            expect(res.body).to.have.property("message", "Invalid username or password");
+        });
+
+        it("should return an error for an incorrect password", async() => {
+            const res = await chai
+                .request(app.app)
+                .post("/login")
+                .send({ username: "testuser1", password: "wrongpassword1" });
+
+            expect(res).to.have.status(400);
+            expect(res.body).to.have.property("message", "Invalid username or password");
+        });
     });
-  });
 });
